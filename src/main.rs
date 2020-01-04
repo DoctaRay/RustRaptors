88978a6b1bc3f92fc353560e315aa02bb295942f
Md Raiyan bin Sayeed <mdraiyanbinsayeed@Mds-MacBook-Pro.local>
2020-01-03 21:40:59 -0500

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
-#[macro_use]
-extern crate serde;
-extern crate serde_derive;
+use std::collections::HashMap;
+use scraper::{Html, Selector};
 extern crate reqwest;
-use reqwest::Error;
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32,
-}
+extern crate tokio;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-                              owner = "rust-lang-nursery",
-                              repo = "rust-cookbook");
-    println!("{}", request_url);
-    let mut response = reqwest::get(&request_url)
+async fn main() -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
+    let html = reqwest::get("https://www.foxsports.com/nba/toronto-raptors-team")
         .await?
         .text()
         .await?;
+    //println!("{:#?}", html);
+    let fragment = Html::parse_fragment(&html);
+    let enemy_selector = Selector::parse(r#"div.wisbb_scoresNavigatorChip:nth-child(6) > div:nth-child(3)"#).unwrap();
+    let score_selector = Selector::parse(r#"div.wisbb_scoresNavigatorChip:nth-child(6) > div:nth-child(4)"#).unwrap();
 
-    let users: Vec<User> = response.json()?;
-    println!("{:?}", users);
+    let enemy = fragment.select(&enemy_selector).next().unwrap().text().collect::<Vec<_>>();
+    let score = fragment.select(&score_selector).next().unwrap().text().collect::<Vec<_>>();
+
+    print!("Toronto Raptors {} {} took a(n) {}: {}", enemy[1].trim(), enemy[2].trim(), score[1].trim(), score[2].trim());
     Ok(())
 }
